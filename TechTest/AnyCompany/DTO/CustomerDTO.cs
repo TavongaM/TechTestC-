5d8baee070c1588c15360f245af656de5675a354
Tavonga Makumbe <T_Makumbe@yahoo.com>
2023-08-23 21:59:49 +0200

    Test Commit

---
+++ TechTest/AnyCompany/DTO/CustomerDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnyCompany.DTO
+{
+    public class CustomerDTO
+    {
+        public CustomerDTO() { }
+
+        public CustomerDTO(string country, DateTime dateOfBirth, string name)
+        {
+            Name = name;
+            Country = country;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+
+
+    }
+}
---
+++ TechTest/AnyCompany/DTO/OrderDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyCompany.DTO
+{
+    public class OrderDTO
+    {
+        public OrderDTO() { }
+
+        public OrderDTO(double amount, double vat)
+        {
+            Amount = amount;
+            VAT = vat;
+        }
+
+        public double Amount { get; set; }
+        public double VAT { get; set; }
+        internal int CustomerId { get; set; }
+    }
+}
